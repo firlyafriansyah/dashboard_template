cfc1c88ce8af85bcd4e2072a2950295e81921f3c
toyoinkdeveloper <70530555+firlyafriansyah@users.noreply.github.com>
2024-02-06 15:56:39 +0700

    Setup project
    Setup project with vite, typescript, eslint, shadcn, tailwind-css, husky, prettier, & react-router-dom

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    extraFileExtensions: ['.css'],
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [1,
+      {
+        extensions: [
+          '.tsx',
+        ],
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'linebreak-style': 0,
+    'no-nested-ternary': 0,
+  },
+};
