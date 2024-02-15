19ca1d5e3efe381c32d8213233c8e38fff6c64b5
toyoinkdeveloper <70530555+firlyafriansyah@users.noreply.github.com>
2024-02-15 10:32:26 +0700

    Design notification pages & fix design header section

--- src/components/custom/notification-dropdown.tsx
+++ src/components/custom/notification-dropdown.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import dayjs from 'dayjs';
 import { useNavigate } from 'react-router-dom';
 import NotificationData from '@/data/notification';
@@ -14,7 +13,7 @@ type NotificationDropdownProps = {
   children: string | JSX.Element | JSX.Element[];
 };
 
-type NotificationItemsProps = {
+type ItemsProps = {
   id: number;
   date: string;
   title: string;
@@ -39,13 +38,13 @@ export default function NotificationDropdown({
         <div className="w-80 h-96 px-0 pt-4 pb-0 animate-header-comp-384">
           <div className="h-80 overflow-y-auto p-0">
             {NotificationData.sort(
-              (a: NotificationItemsProps, b: NotificationItemsProps) => parseInt(dayjs(b.date).format(), 10)
+              (a: ItemsProps, b: ItemsProps) => parseInt(dayjs(b.date).format(), 10)
                 - parseInt(dayjs(a.date).format(), 10),
             )
               .sort(
-                (a: NotificationItemsProps, b: NotificationItemsProps) => (a.read ? 1 : 0) - (b.read ? 1 : 0),
+                (a: ItemsProps, b: ItemsProps) => (a.read ? 1 : 0) - (b.read ? 1 : 0),
               )
-              .map((items: NotificationItemsProps) => (
+              .map((items: ItemsProps) => (
                 <NotificationItems
                   key={items.id}
                   id={items.id}
